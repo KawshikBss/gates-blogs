7d31540f90c2b04d0cfa157b0fe6a5e2b1a52231
KawshikBss <kawshikbss@gmail.com>
2023-03-10 00:40:05 +0600

    completed footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,5 +5,5 @@
             @include('components/subscribe')
         </span>
     </span>
-    <p class="footer-contnet">Gates is a blog that focuses on Japanese art and anime. They feature information on Otaku conventions and other anime topics. Hear from famous anime filmmakers and artists as well as plenty of news about the art of cartoon creation in the Japanese culture.</p>
+    <p class="footer-content">Gates is a blog that focuses on Japanese art and anime. They feature information on Otaku conventions and other anime topics. Hear from famous anime filmmakers and artists as well as plenty of news about the art of cartoon creation in the Japanese culture.</p>
 </div>
