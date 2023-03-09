ca0d13ab6288e65c357f524561bc2c541826062a
KawshikBss <kawshikbss@gmail.com>
2023-03-10 02:40:18 +0600

    modified navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,20 @@
 <div class="navbar-container">
-    <img src="{{ asset('images/logo.png') }}" class="navbar-logo" />
+    <a href="/"><img src="{{ asset('images/logo.png') }}" class="navbar-logo" /></a>
     <div class="navbar-links">
         <a href="/" class="navbar-navlink navlink-active">Home</a>
         <a href="#" class="navbar-navlink">Connect</a>
-        <a href="#" class="navbar-navlink">Sign up</a>
-        <a href="#" class="navbar-navlink">Login</a>
+        @if(auth()->user())
+        <span>{{auth()->user()->name}}</span>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+            @csrf
+
+            <button class="navbar-navlink" type="submit">
+                Logout
+            </button>
+        </form>
+        @else
+        <a href="{{route('signup')}}" class="navbar-navlink">Sign up</a>
+        <a href="{{route('login')}}" class="navbar-navlink">Login</a>
+        @endif
     </div>
 </div>
