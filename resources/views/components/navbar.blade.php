09526139e09442fc9942d6c83b5e2ab4483fc32d
KawshikBss <kawshikbss@gmail.com>
2023-03-09 20:40:56 +0600

    created navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,9 @@
+<div class="navbar-container">
+    <img src="{{ asset('images/logo.png') }}" class="navbar-logo" />
+    <div class="navbar-links">
+        <a href="/" class="navbar-navlink">Home</a>
+        <a href="#" class="navbar-navlink">Connect</a>
+        <a href="#" class="navbar-navlink">Sign up</a>
+        <a href="#" class="navbar-navlink">Login</a>
+    </div>
+</div>
