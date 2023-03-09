41b7fa1d59ffec4c736e9bee33acaf1708aff335
KawshikBss <kawshikbss@gmail.com>
2023-03-09 20:41:14 +0600

    created component for subscribe

---
+++ resources/views/components/subscribe.blade.php
@@ -0,0 +1,4 @@
+<span class="banner-sub-field">
+    <button class="banner-sub-btn">Subscribe</button>
+    <input type="text" name="" id="" class="banner-sub-input" placeholder="Enter Your Email" />
+</span>
