cf08b71018e6e606d2f0b4eef4f3c1ce061e9a4e
KawshikBss <kawshikbss@gmail.com>
2023-03-10 00:40:25 +0600

    added active class

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="navbar-container">
     <img src="{{ asset('images/logo.png') }}" class="navbar-logo" />
     <div class="navbar-links">
-        <a href="/" class="navbar-navlink">Home</a>
+        <a href="/" class="navbar-navlink navlink-active">Home</a>
         <a href="#" class="navbar-navlink">Connect</a>
         <a href="#" class="navbar-navlink">Sign up</a>
         <a href="#" class="navbar-navlink">Login</a>
