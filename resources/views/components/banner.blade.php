354b3938b612c6f965c60a5b88c6983106a5b4f0
KawshikBss <kawshikbss@gmail.com>
2023-03-09 20:40:28 +0600

    created header

---
+++ resources/views/components/banner.blade.php
@@ -0,0 +1,15 @@
+<div class="banner-container">
+    <div class="banner-content">
+        <h3 class="banner-heading">Welcome To Gates</h3>
+        <p class="banner-description">Get the latest news on your favourite mangas, anime and manhwa around the world!</p>
+        @include('components/subscribe')
+    </div>
+    <img src="{{asset('images/bannerInner.png')}}" class="banner-inner-img" />
+    <ul class="menu-items">
+        <li class="menu-item">Action</li>
+        <li class="menu-item">Comedy</li>
+        <li class="menu-item">Drama</li>
+        <li class="menu-item">Military</li>
+        <li class="menu-item">Fantasy</li>
+    </ul>
+</div>
