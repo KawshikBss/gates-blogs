cf81e620fced7ee75d5f53dfef707086efef2112
KawshikBss <kawshikbss@gmail.com>
2023-03-10 19:04:21 +0600

    completed login form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,10 @@
 
 @section('content')
 
-Login
+<x-form-layout :fields="[
+    ['name' => 'email', 'label' => 'Email', 'type' => 'email', 'value' => old('email'), 'placeholder' => 'Enter Your Email',],
+    ['name' => 'password', 'label' => 'Password', 'type' => 'password', 'placeholder' => 'Enter Your Password',],
+    ]" :submitLabel="'Login'" method="{{'POST'}}" action="{{ route('login') }}">
+</x-form-layout>
 
 @endsection
