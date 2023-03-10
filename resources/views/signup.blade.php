6248ff4505e5e557d59f7bde3721db4b5719232b
KawshikBss <kawshikbss@gmail.com>
2023-03-10 19:04:33 +0600

    completed signup form

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -4,68 +4,11 @@
 
 @section('content')
 
-Signup
-<form method="POST" action="{{ route('signup') }}">
-    @csrf
-
-    <div>
-        <label for="name">Name</label>
-
-        <div>
-            <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
-
-            @error('name')
-            <span>
-                <strong>{{ $errors->first('name') }}</strong>
-            </span>
-            @enderror
-        </div>
-    </div>
-
-    <div>
-        <label for="email">Email</label>
-
-        <div>
-            <input id="email" type="email" name="email" value="{{ old('email') }}" required>
-
-            @error('email')
-            <span>
-                <strong>{{ $errors->first('email') }}</strong>
-            </span>
-            @enderror
-        </div>
-    </div>
-
-    <div>
-        <label for="password">Password</label>
-
-        <div>
-            <input id="password" type="password" name="password" required>
-
-            @error('password')
-            <span>
-                <strong>{{ $errors->first('password') }}</strong>
-            </span>
-            @enderror
-        </div>
-    </div>
-
-    <div>
-        <label for="password_confirmation">Confirm Password</label>
-
-        <div>
-            <input id="password_confirmation" type="password" name="password_confirmation" required>
-        </div>
-    </div>
-
-    <div>
-        <div>
-            <button type="submit">
-                Register
-            </button>
-        </div>
-    </div>
-</form>
-
+<x-form-layout :fields="[
+    ['name' => 'name', 'label' => 'Name', 'type' => 'text', 'value' => old('name'), 'placeholder' => 'Enter Your Name',],
+    ['name' => 'email', 'label' => 'Email', 'type' => 'email', 'value' => old('email'), 'placeholder' => 'Enter Your Email',],
+    ['name' => 'password', 'label' => 'Password', 'type' => 'password', 'placeholder' => 'Enter Your Password',],
+    ]" :submitLabel="'Sign Up'" method="{{'POST'}}" action="{{ route('signup') }}">
+</x-form-layout>
 
 @endsection
