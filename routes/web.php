096229dacaf27b8352515ceba63ec6dc9d33c60b
KawshikBss <kawshikbss@gmail.com>
2023-03-10 19:04:47 +0600

    added login routes

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,6 @@ Route::get('/signup', [SignUpController::class, 'showSignUpForm'])->name('signup
 Route::post('/signup', [SignUpController::class, 'signup']);
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
