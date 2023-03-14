eb2e402aaf4180339dde43040120d48984f72bc6
KawshikBss <kawshikbss@gmail.com>
2023-03-14 20:00:33 +0600

    added error messages

--- resources/views/components/form-layout.blade.php
+++ resources/views/components/form-layout.blade.php
@@ -3,7 +3,7 @@
 @endpush
 <div class="form-container">
 
-    <form method="{{ $method }}" action="{{ $action }}" class="form-inner">
+    <form method="{{ $method }}" action="{{ $action }}" enctype="multipart/form-data" class="form-inner">
         @csrf
 
         @foreach($fields as $field)
@@ -11,7 +11,10 @@
         <div class="form-input-field">
             <label for="{{$field['name']}}" class="form-input-label">{{$field['label']}}</label>
 
-            <input class="form-input" id="{{$field['name']}}" type="{{$field['type']}}" name="{{$field['name']}}" value="{{$field['value']?? '' }}" required placeholder="{{$field['placeholder']}}" />
+            <input class="form-input" id="{{$field['name']}}" type="{{$field['type']}}" accept="{{$field['accept']?? ''}}" name="{{$field['name']}}" value="{{$field['value']?? '' }}" required placeholder="{{$field['placeholder']?? ''}}" />
+            @error($field['name'])
+            <p>{{$message}}</p>
+            @enderror
 
         </div>
 
