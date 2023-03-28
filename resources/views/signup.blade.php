01bed4ec2053dafccfdd758ffea0fac9177fd9a9
KawshikBss <kawshikbss@gmail.com>
2023-03-28 21:43:19 +0600

    fixed

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -4,10 +4,16 @@
 
 @section('content')
 
+@section('content')
+@if($errors->has('message'))
+<span>{{$errors->first('message')}}</span>
+@endif
+
 <x-form-layout :fields="[
     ['name' => 'name', 'label' => 'Name', 'type' => 'text', 'value' => old('name'), 'placeholder' => 'Enter Your Name',],
     ['name' => 'email', 'label' => 'Email', 'type' => 'email', 'value' => old('email'), 'placeholder' => 'Enter Your Email',],
     ['name' => 'password', 'label' => 'Password', 'type' => 'password', 'placeholder' => 'Enter Your Password',],
+    ['name' => 'password_confirmation', 'label' => 'Confirm Password', 'type' => 'password', 'placeholder' => 'Confirm Your Password',],
     ]" :submitLabel="'Sign Up'" method="{{'POST'}}" action="{{ route('signup') }}">
 </x-form-layout>
 
