8e1a020f9a1ec25fe24c603594098e63bf4f585e
KawshikBss <kawshikbss@gmail.com>
2023-03-28 21:40:48 +0600

    added error message

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,9 @@
 @section('title', 'Gates - Login')
 
 @section('content')
-
+@if($errors->has('message'))
+<span>{{$errors->first('message')}}</span>
+@endif
 <x-form-layout :fields="[
     ['name' => 'email', 'label' => 'Email', 'type' => 'email', 'value' => old('email'), 'placeholder' => 'Enter Your Email',],
     ['name' => 'password', 'label' => 'Password', 'type' => 'password', 'placeholder' => 'Enter Your Password',],
